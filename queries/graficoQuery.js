97811ad7a346e6022cc415943eb8de8cf55be863
vitorsilvalima <vitor.lima2@fatec.sp.gov.br>
2016-10-24 19:17:45 -0200

    added another query to the graficos router

--- queries/graficoQuery.js
+++ queries/graficoQuery.js
@@ -6,27 +6,30 @@ var db = require('../database/postgresqlDB');
 function all(req, res, next){
      //    console.log(req.params);
     var retorno = {
-            atendimentoAno:0,
-            atendimentoMes:0
     };
     db.tx(function(t) {   
-        return t.one("select count(*) as total_atendimentos from atendimento "
+        return t.oneOrNone("select count(*) as total_atendimentos from atendimento "
                           +"where extract(year from data_inicio)=${ano}",req.params)
         .then(data=>{
             retorno.atendimentoAno=parseInt(data.total_atendimentos);
-            return t.one("select count(*) as total_mes from atendimento "
-                          +"where extract(year from data_inicio)=${ano}",req.params);
+            return t.oneOrNone("select count(*) as total_mes from atendimento "
+                          +"where extract(year from data_inicio)=${ano} "
+                          +"AND extract(MONTH from data_inicio)=${mes}",req.params);
+        })
+        .then(data=>{
+            retorno.atendimentoMes=parseInt(data.total_mes);
+            return t.any("select count(*) as total, "
+            +"(select nome from usuario where usuario.usuario_id=atendimento.usuario_id) "
+            +"from atendimento  where extract(year from data_inicio)=${ano} "
+            +"AND extract(MONTH from data_inicio)=${mes} "
+            +"GROUP BY usuario_id  ORDER BY total DESC",req.params);
         });
     })
     .then(function(data) {
-        retorno.atendimentoMes=parseInt(data.total_mes);
+        retorno.destaques=data;
         console.log(retorno);
         res.status(200)
-        .json({
-          status: true,
-          data:retorno,
-          message: 'Atualizado com sucesso'
-        });
+        .json(retorno);
     })
     .catch(function(error) {
         // error
