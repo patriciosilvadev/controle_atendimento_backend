8ed901812a55a5cddff231e78fc00e54860afdfa
vitorsilvalima <vitor.lima2@fatec.sp.gov.br>
2016-11-25 22:57:34 -0200

    added date

--- routes/atendimentoRouter.js
+++ routes/atendimentoRouter.js
@@ -6,6 +6,7 @@ var db_atendimento = require('../queries/atendimentoQuery');
 //router.post('/api/atendimentos', db_atendimento.create);
 router.post('/api/atendimentos', db_atendimento.insert);
 router.get('/api/atendimentos', db_atendimento.all);
+router.get('/api/atendimentos/:ano/:mes', db_atendimento.allByMonth);
 router.put('/api/atendimentos/finalizar/:atendimento_id', db_atendimento.finalizar);
 router.put('/api/atendimentos/:atendimento_id', db_atendimento.update);
 
