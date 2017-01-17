c0e62a77cf247afee21bb6439c1fc86fc40a1f56
vitorsilvalima <vitor.lima2@fatec.sp.gov.br>
2017-01-17 15:49:58 -0200

    fixed error with dates and added debug library

--- queries/utilQ.js
+++ queries/utilQ.js
@@ -1,6 +1,6 @@
-
-
 var utilModel = {};
+var debug = require('debug')('utilQ');
+
 
 utilModel.getDates = function(ano,mes,dia){
     
@@ -8,7 +8,7 @@ utilModel.getDates = function(ano,mes,dia){
 
     //ano
     returnDates.ano_inicio=new Date(ano,0,1)
-    returnDates.ano_fim=new Date(ano,0,0);
+    returnDates.ano_fim=new Date(ano+1,0,0);
 
     //mes
     returnDates.mes_inicio=new Date(ano,mes,1)
@@ -17,9 +17,15 @@ utilModel.getDates = function(ano,mes,dia){
     //semana
     var semana=new Date(ano,mes,dia);
     returnDates.semana_inicio=new Date(ano,mes,semana.getDate()-semana.getDay());
-    returnDates.semana_fim=new Date(ano,mes+1,semana.getDate()+6);
+    returnDates.semana_fim=new Date(returnDates.semana_inicio.getFullYear(),returnDates.semana_inicio.getMonth(),returnDates.semana_inicio.getDate()+6);
+
+    debug('As datas de pesquisa sao: '
+    +'\n Ano= %s - %s'
+    +'\n Mes= %s - %s'
+    +'\n Semana= %s - %s',returnDates.ano_inicio,returnDates.ano_fim,returnDates.mes_inicio,returnDates.mes_fim,returnDates.semana_inicio,returnDates.semana_fim);
 
     return returnDates;
+
 
 };
 
